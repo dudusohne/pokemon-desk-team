c3bb8016a19340fa323cea170f1a0970b411e9d1
dudusohne <dudu.hit@gmail.com>
2022-05-02 19:16:09 -0300

    fix env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig: any = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: "pokemondesk-67ce8",
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
