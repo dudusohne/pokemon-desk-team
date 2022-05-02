2e168d09bca82bd45df4feaf89510a40ee694e81
dudusohne <dudu.hit@gmail.com>
2022-05-02 09:58:35 -0300

    env variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBe3hO9A9GJyxNZR1LOQs-s9X8540ejfGA",
-  authDomain: "pokemondesk-67ce8.firebaseapp.com",
-  projectId: "pokemondesk-67ce8",
-  storageBucket: "pokemondesk-67ce8.appspot.com",
-  messagingSenderId: "754573502548",
-  appId: "1:754573502548:web:2995c47dcf605ee2b1a5c1",
-  measurementId: "G-4Z6E3BRRLQ"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
