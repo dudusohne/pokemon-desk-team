03bff42a25b277d508785c9b23c77bc5f16fd855
Eduardo Sohne <dudu.hit@gmail.com>
2022-05-02 21:40:24 -0300

    few ajusts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig: any = {
   apiKey: "AIzaSyBe3hO9A9GJyxNZR1LOQs-s9X8540ejfGA",
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  authDomain: "pokemondesk-67ce8.firebaseapp.com",
   projectId: "pokemondesk-67ce8",
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
