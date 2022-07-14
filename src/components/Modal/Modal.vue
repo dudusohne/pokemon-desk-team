da6e690846ef749032250aae7d8ed81c143fe809
Eduardo Sohne <dudu.hit@gmail.com>
2022-07-13 21:05:18 -0300

    change poke img css mobile

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -3,15 +3,13 @@
         <q-card class="text-white">
             <div :style="{ 'background-color': props.pokemon.color, overflow: 'hidden' }">
                 <img class="pokedeximgheader" src="../../assets/headerpokedex.png" alt="pokedex" />
-                <q-bar
-                    style="display: flex; flex-direction: row; justify-content: space-between; align-items: center"
-                >
+                <q-bar style="display: flex; flex-direction: row; justify-content: space-between; align-items: center">
                     <q-btn rounded>
                         <span style="font-size: 25px; font-weight: bold;">#{{ props.pokemon.id }}</span>
                     </q-btn>
-                    <span
-                        style="align-self: flex-start; font-size: 28px; font-weight: bold; color: rgb(31, 31, 31);"
-                    >{{ props.pokemon.name }}</span>
+                    <span style="align-self: flex-start; font-size: 28px; font-weight: bold; color: rgb(31, 31, 31);">{{
+                            props.pokemon.name
+                    }}</span>
 
                     <q-btn dense flat icon="close" v-close-popup>
                         <q-tooltip class="bg-white text-primary">Close</q-tooltip>
@@ -21,20 +19,14 @@
                 <q-card-section>
                     <div style="display: flex; flex-direction: column; align-items: center">
                         <div></div>
-                        <img
-                            :src="getPokemonImg(props.pokemon.id)"
-                            alt="image"
-                            style="max-width: 300px"
-                        />
-                        <div
-                            style="display: flex; flex-direction: row; margin-bottom: 10px; align-items: flex-start"
-                        >
+                        <img class="pokemon-img" :src="getPokemonImg(props.pokemon.id)" alt="image" />
+                        <div style="display: flex; flex-direction: row; margin-bottom: 10px; align-items: flex-start">
                             <InfoCard>
                                 <span style="color: rgb(189, 189, 189)">POWER:</span>
                                 <q-separator />
-                                <p
-                                    style="font-size: 28px; font-weight: bold; color: rgb(255, 255, 255)"
-                                >{{ props.pokemon.base_experience }}</p>
+                                <p style="font-size: 28px; font-weight: bold; color: rgb(255, 255, 255)">{{
+                                        props.pokemon.base_experience
+                                }}</p>
                             </InfoCard>
 
                             <InfoCard>
@@ -47,11 +39,9 @@
                                 <span style="color: rgb(189, 189, 189)">TYPE(S):</span>
                                 <q-separator />
                                 <div v-for="typ in props.pokemon.types">
-                                    <q-chip
-                                        class="flat"
-                                        :color="props.pokemon.color"
-                                        text-color="white"
-                                    >{{ typ.type.name }}</q-chip>
+                                    <q-chip class="flat" :color="props.pokemon.color" text-color="white">{{
+                                            typ.type.name
+                                    }}</q-chip>
                                 </div>
                             </InfoCard>
 
@@ -61,26 +51,20 @@
                                 <div>
                                     <p>
                                         <span
-                                            style="font-weight: 400; color: rgb(168, 168, 168); margin-right: 4px"
-                                        >HEIGHT:</span>
+                                            style="font-weight: 400; color: rgb(168, 168, 168); margin-right: 4px">HEIGHT:</span>
                                         <span>{{ props.pokemon.height }}</span>
                                     </p>
                                     <p>
                                         <span
-                                            style="font-weight: 400; color: rgb(168, 168, 168); margin-right: 4px"
-                                        >WEIGHT:</span>
+                                            style="font-weight: 400; color: rgb(168, 168, 168); margin-right: 4px">WEIGHT:</span>
                                         <span>{{ props.pokemon.weight }}</span>
                                     </p>
                                 </div>
                             </InfoCard>
                         </div>
-                        <q-btn
-                            dense
-                            style="padding: 10px 10px 0px 10px; background-color: rgb(88, 88, 88)"
-                        >
+                        <q-btn dense style="padding: 10px 10px 0px 10px; background-color: rgb(88, 88, 88)">
                             <div
-                                style="display: flex; flex-direction: column; align-items: flex-start; justify-content: center;"
-                            >
+                                style="display: flex; flex-direction: column; align-items: flex-start; justify-content: center;">
                                 <p style="color: rgb(223, 223, 223)">{{ props.pokemon.description }}</p>
                             </div>
                         </q-btn>
@@ -129,5 +113,13 @@ emit('clicked')
     height: auto;
     margin-left: -1rem;
     margin-top: -1rem;
+}
+
+.pokemon-img {
+    max-width: 300px;
+
+    @media (max-width: 468px) {
+      max-width: 200px;
+    }
 }
 </style>
