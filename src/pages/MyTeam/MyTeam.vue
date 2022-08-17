9aac69d64430e79b947eaf7cd18846ba0853113a
Eduardo Sohne <dudu.hit@gmail.com>
2022-08-16 22:21:17 -0300

    change my team logic

--- src/pages/MyTeam/MyTeam.vue
+++ src/pages/MyTeam/MyTeam.vue
@@ -14,21 +14,23 @@
         <p>340</p>
       </div>
       <q-separator color="green" />
-      <!-- <span class="container-desc">you can roll and get a new team</span>
-      <q-btn label="ROLL" color="yellow" text-color="black" style="margin-top: 1rem" /> -->
+      <div v-show="newTeam.length < 5" style="display: flex; flex-direction: column;">
+        <span class="container-desc">you can roll and get a new team</span>
+        <q-btn label="ROLL" color="yellow" text-color="black" style="margin-top: 1rem" @click="generateRandomTeam()" />
+      </div>
     </div>
     <BottomBar />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { getDatabase, ref as Ref, get, child } from "firebase/database";
+import { ref, watch } from "vue";
+import { getDatabase, ref as Ref, get, set, child } from "firebase/database";
 import { getAuth } from "firebase/auth";
-import { useRoute } from "vue-router";
 import Header from '../../components/Header/Header.vue'
 import TeamPokeCard from "../../components/TeamPokeCard/TeamPokeCard.vue";
 import BottomBar from "../../components/BottomBar/BottomBar.vue"
+import { useRouter, useRoute } from "vue-router";
 
 const resultData = ref()
 
@@ -49,6 +51,63 @@ get(child(dbRef, `users/${auth.currentUser?.uid}`)).then((snapshot) => {
 }).catch((error) => {
   console.error(error);
 });
+
+const newTeam = ref<any>([])
+
+function generateRandomTeam() {
+  for (var i = 1; i <= 6; i++) {
+    getRandomIntNoDuplicates(1, 10, newTeam.value)
+  }
+
+  function inArray(arr: any, el: any) {
+    for (var i = 0; i < arr.length; i++)
+      if (arr[i] == el) return true;
+    return false;
+  }
+
+  function getRandomIntNoDuplicates(min: any, max: any, newTeamArr: any): any {
+    var RandomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (newTeamArr.length > (max - min)) return false;  // break endless recursion
+    if (!inArray(newTeamArr, RandomInt)) {
+      newTeam.value.push(RandomInt);
+      console.log(newTeam.value);
+    }
+  }
+
+  saveTeam()
+}
+
+function saveTeam() {
+  const user = auth.currentUser;
+  const userId: any = user?.uid;
+  const userEmail: string | any = user?.email;
+  const userName = user?.displayName;
+  const userPhoto = user?.photoURL;
+  const userTeam: any = newTeam.value;
+
+  const power = ref()
+
+  power.value = userTeam.reduce((result: any, item: any) => {
+    return result + item?.base_experience
+  }, 0)
+
+  const db = getDatabase();
+
+  try {
+    set(Ref(db, `users/` + userId), {
+      email: userEmail,
+      name: userName,
+      photo: userPhoto,
+      team: userTeam,
+      power: power.value
+    });
+
+    alert('NEW TEAM SAVED')
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 </script>
 
 <style lang="scss" scoped>
